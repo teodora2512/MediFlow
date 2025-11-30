5f7768524cb74cc29da90d8ddb4105aa33d3749c
teodora2512 <teodora.otelariu@student.upt.ro>
2025-12-01 00:07:12 +0200

    Refactor(Repository): Trecere la Optional<Utilizator> pentru findByUsername

--- gestiune-pacienti/src/main/java/ro/univ/medical/gestiune_pacienti/repository/UtilizatorRepository.java
+++ gestiune-pacienti/src/main/java/ro/univ/medical/gestiune_pacienti/repository/UtilizatorRepository.java
@@ -3,6 +3,8 @@ package ro.univ.medical.gestiune_pacienti.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.univ.medical.gestiune_pacienti.model.Utilizator;
 
+import java.util.Optional;
+
 public interface UtilizatorRepository extends JpaRepository<Utilizator, Integer> {
-    Utilizator findByUsername(String username);
+    Optional<Utilizator> findByUsername(String username);
 }
